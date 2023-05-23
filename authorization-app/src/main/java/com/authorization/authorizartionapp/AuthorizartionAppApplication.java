0d8e428ee47b90d973fa1f6a585a79298380fea9
Elkin Aguilar <aguilar.elkin99@gmail.com>
2023-05-22 21:32:51 -0500

    Create Authorization server OAUTH2

---
+++ authorization-app/src/main/java/com/authorization/authorizartionapp/AuthorizartionAppApplication.java
@@ -0,0 +1,13 @@
+package com.authorization.authorizartionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizartionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizartionAppApplication.class, args);
+	}
+
+}
---
+++ authorization-app/src/test/java/com/authorization/authorizartionapp/AuthorizartionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authorization.authorizartionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizartionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
